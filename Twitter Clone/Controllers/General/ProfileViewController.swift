5fbdc61482fd24e9200c886dcef0a38803b761b1
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-03-23 09:16:44 +0000

    Add profile Header View

--- Twitter Clone/Controllers/General/ProfileViewController.swift
+++ Twitter Clone/Controllers/General/ProfileViewController.swift
@@ -9,13 +9,51 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    let profileTableView: UITableView = {
+        let table = UITableView()
+        table.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
       
         view.backgroundColor = .systemBackground
         navigationItem.title = "Profile"
+        
+        let headerView = ProfileTableViewHeader(frame: CGRect(x: 0, y: 0, width: profileTableView.frame.width, height: 380))
+        
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+        view.addSubview(profileTableView)
+        profileTableView.tableHeaderView = headerView
+        addConstraints()
+       
+        
+    }
+ 
+    
+}
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+}
 
+extension ProfileViewController {
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            profileTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            profileTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
---
+++ Twitter Clone/View/ProfileTableViewHeader.swift
@@ -0,0 +1,130 @@
+//
+//  ProfileTableViewHeader.swift
+//  Twitter Clone
+//
+//  Created by Vladlens Kukjans on 23/03/2023.
+//
+
+import UIKit
+
+class ProfileTableViewHeader: UIView {
+    
+    private let joinedDateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.text = "Joined March 2023"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14,weight: .regular)
+        return label
+    }()
+    
+    private let joinDateImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "calendar",withConfiguration: UIImage.SymbolConfiguration(pointSize: 14))
+        imageView.tintColor = .secondaryLabel
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    private let userBioLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 3
+        label.textColor = .label
+        label.text = "iOS Developer"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+        
+    private let userNameLabel: UILabel = {
+        let lable = UILabel()
+        lable.text = "@vladlenskukjans"
+        lable.textColor = .secondaryLabel
+        lable.font = .systemFont(ofSize: 18, weight: .regular)
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        return lable
+    }()
+    
+    
+    private let displayNameLabel: UILabel = {
+        let lable = UILabel()
+        lable.text = "Vladlens Kukjans"
+        lable.font = .systemFont(ofSize: 22, weight: .bold)
+        lable.textColor = .label
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        return lable
+    }()
+    
+    
+    private let profileAvatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 40
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .green
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let profileHeaderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "header")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(profileHeaderImageView)
+        addSubview(profileAvatarImageView)
+        addSubview(displayNameLabel)
+        addSubview(userNameLabel)
+        addSubview(userBioLabel)
+        addSubview(joinDateImageView)
+        addSubview(joinedDateLabel)
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
+extension ProfileTableViewHeader {
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            profileHeaderImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            profileHeaderImageView.topAnchor.constraint(equalTo: topAnchor),
+            profileHeaderImageView.trailingAnchor.constraint(equalTo:trailingAnchor),
+            profileHeaderImageView.heightAnchor.constraint(equalToConstant: 180),
+            
+            profileAvatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            profileAvatarImageView.centerYAnchor.constraint(equalTo: profileHeaderImageView.bottomAnchor,constant: 10),
+            profileAvatarImageView.widthAnchor.constraint(equalToConstant: 80),
+            profileAvatarImageView.heightAnchor.constraint(equalToConstant: 80),
+            
+            displayNameLabel.leadingAnchor.constraint(equalTo: profileAvatarImageView.leadingAnchor),
+            displayNameLabel.topAnchor.constraint(equalTo: profileAvatarImageView.bottomAnchor, constant: 18),
+            
+            userNameLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            userNameLabel.topAnchor.constraint(equalTo: displayNameLabel.bottomAnchor, constant: 8),
+            
+            userBioLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            //userBioLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            userBioLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
+            
+            joinDateImageView.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            joinDateImageView.topAnchor.constraint(equalTo: userBioLabel.bottomAnchor, constant: 8),
+            
+            joinedDateLabel.leadingAnchor.constraint(equalTo: joinDateImageView.trailingAnchor, constant: 2),
+            joinedDateLabel.centerYAnchor.constraint(equalTo: joinDateImageView.centerYAnchor)
+
+        ])
+    }
+}
