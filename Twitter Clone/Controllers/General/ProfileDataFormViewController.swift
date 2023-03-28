dafe494333054c976e8715724ab6e10dcc52b7f8
Vladlens Kukjans <kukjanvladlen@gmail.com>
2023-03-28 16:05:01 +0100

    add Profile data form VC

--- Twitter Clone/Controllers/General/ProfileDataFormViewController.swift
+++ Twitter Clone/Controllers/General/ProfileDataFormViewController.swift
@@ -10,13 +10,35 @@ import UIKit
 class ProfileDataFormViewController: UIViewController {
     
     
-    
+    let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.alwaysBounceVertical = true
+        scroll.keyboardDismissMode = .onDrag
+        scroll.translatesAutoresizingMaskIntoConstraints = true
+        return scroll
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        addConstraints()
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.frame = view.bounds
+        
+    }
 
+}
+extension ProfileDataFormViewController {
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+        
+        
+        ]
+        )
+    }
 }
--- Twitter Clone/ViewModel/ProfileDataFormViewModel.swift
+++ Twitter Clone/ViewModel/ProfileDataFormViewModel.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ProfileDataFormViewModel: ObservableObject {
+    
+    
+    
 
    
     @Published var displayName: String?
